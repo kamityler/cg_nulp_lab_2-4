9dad6f52ca6f10bee1a664e1b12f0e70f6ebfede
Danylo <bodnar.dp@gmail.com>
2022-11-07 01:11:14 +0200

    second lab

---
+++ js/harter.js
@@ -0,0 +1,48 @@
+function dragonSequence(n) {
+    var seq = [1];
+    for (let i = 0, a = 0; i < n; i++, a = a ? 0 : 1) {
+      seq = seq.concat(a, seq.reverse().map(s => s ? 0 : 1).reverse());
+    }
+    return seq;
+  }
+  
+  function setup() {
+    let myCanvas = createCanvas(650, 650);
+    myCanvas.parent("harterfrac");
+    noLoop();
+    doLoop=false;
+    colorMode(HSB);
+    background(220,220,220);
+  }
+  
+  function generate(){
+    iterations = select("#iterNumber").value();
+    dragon = dragonSequence(iterations);
+    l = dragon.length;
+    x = width / 2;
+    y = width / 2;
+    d = 0; 
+    s = 1/select("#scaleNumber").value();
+    index = 1;
+    doLoop=true;
+    loop();
+    background(220,0,0);
+  }
+  
+  function draw() {
+    if(!doLoop) return;
+    
+    for (let k = 0; k < constrain(l/60,0,1000); k++) {
+      d += dragon[index] * 2 - 1;
+      var nx = x + cos(TAU / 4 * d) * s;
+      var ny = y + sin(TAU / 4 * d) * s;
+  
+      stroke(360/l*index,127,127);
+      line(x, y, nx, ny);
+      x = nx;
+      y = ny;
+  
+      index++;
+      if (index > dragon.length) noLoop();
+    }
+  }
---
+++ js/koch.js
@@ -0,0 +1,116 @@
+let k;
+
+function setup() {
+  let myCanvas = createCanvas(870, 650);
+  myCanvas.parent("kochfrac");
+  frameRate(1);  
+  k = new KochFractal();
+  background(255,255,102);
+}
+
+function draw() {
+  background(255,255,102);
+  k.render();
+  k.nextLevel();
+  if (k.getCount() > 5) {
+    k.restart();
+    stroke(51,255,255);
+  }
+
+}
+
+class KochLine {
+  constructor(a,b) {
+    this.start = a.copy();
+    this.end = b.copy();
+  }
+
+  display() { 
+    stroke(0,204,204);
+    line(this.start.x, this.start.y, this.end.x, this.end.y);
+  }
+
+  kochA() {
+    return this.start.copy();
+  }
+
+  kochB() {
+    let v = p5.Vector.sub(this.end, this.start);
+    v.div(3);
+    v.add(this.start);
+    return v;
+  }
+
+  kochC() {
+    let a = this.start.copy();
+    let v = p5.Vector.sub(this.end, this.start);
+    v.div(3);
+    a.add(v);
+    v.rotate(-PI/3);
+    a.add(v);
+    return a;
+  }
+
+
+  kochD() {
+    let v = p5.Vector.sub(this.end, this.start);
+    v.mult(2/3.0);
+    v.add(this.start);
+    return v;
+  }
+
+  kochE() {
+    return this.end.copy();
+  }
+}
+
+
+class KochFractal {
+  constructor() {
+    this.start = createVector(0,height-20);   
+    this.end = createVector(width,height-20); 
+    this.lines = [];                         
+    this.count = 0;
+    this.restart();
+  }
+
+  nextLevel() {
+
+    this.lines = this.iterate(this.lines);
+    this.count++;
+  }
+
+  restart() {
+    this.count = 0;      
+    this.lines = [];  
+    this.lines.push(new KochLine(this.start,this.end));  
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  render() {
+    for(let i = 0; i < this.lines.length; i++) {
+      this.lines[i].display();
+    }
+  }
+
+  iterate(before) {
+    let now = [];    
+    for(let i = 0; i < this.lines.length; i++) {
+      let l = this.lines[i];
+      let a = l.kochA();
+      let b = l.kochB();
+      let c = l.kochC();
+      let d = l.kochD();
+      let e = l.kochE();
+
+      now.push(new KochLine(a,b));
+      now.push(new KochLine(b,c));
+      now.push(new KochLine(c,d));
+      now.push(new KochLine(d,e));
+    }
+    return now;
+  }
+}
